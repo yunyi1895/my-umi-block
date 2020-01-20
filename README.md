46a70644572f01faa8ed53f620b67fb02f350df4
陈明烽 <chenmingfeng@extracme.com>
2020-01-20 13:52:41 +0800

    What: 1. 初始化
    Why: 1.
    
    How: 1.

---
+++ README.md
@@ -0,0 +1,13 @@
+# @umi-material/my-umi-block
+
+测试区块
+
+## Usage
+
+```sh
+umi block https://github.com/umijs/umi-blocks/tree/master/my-umi-block
+```
+
+## LICENSE
+
+MIT
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.css';
+
+const Block: React.FC = () => {
+  return (
+    <div className={styles.normal}>
+      <h1>I am a umi block!wwwww</h1>
+    </div>
+  );
+}
+
+export default Block;
